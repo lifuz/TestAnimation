cc135e1c9e5569ff08d21abbac45390b966501bd
lifuz <lifuzz@163.com>
2016-04-21 13:09:09 +0800

    Initial commit

---
+++ app/src/main/java/com/lifuz/animation/MainActivity.java
@@ -0,0 +1,41 @@
+package com.lifuz.animation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textWidget;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        //监听动画的状态（开始，结束）
+//        anim.setAnimationListener(new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//
+//            }
+//        });
+        textWidget = (TextView) findViewById(R.id.text_widget);
+        textWidget.setText("画面旋转动画效果");
+        textWidget.startAnimation(anim);
+    }
+}
