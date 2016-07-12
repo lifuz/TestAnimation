6e436dd89b7732d61d37e8e86958c85b5016da05
lifuz <lifuzz@163.com>
2016-07-12 21:36:01 +0800

    fragment

---
+++ self/src/main/java/com/lifuz/self/fragment/KnowledgeFragment.java
@@ -0,0 +1,14 @@
+package com.lifuz.self.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ *
+ * 知识模块的fragment
+ *
+ * @author: 李富
+ * @email: lifuzz@163.com
+ * @time: 2016/7/12 21:34
+ */
+public class KnowledgeFragment extends Fragment {
+}
---
+++ self/src/main/java/com/lifuz/self/fragment/WealthFragment.java
@@ -0,0 +1,12 @@
+package com.lifuz.self.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 财富模块的fragment
+ * @author: 李富
+ * @email: lifuzz@163.com
+ * @time: 2016/7/12 21:34
+ */
+public class WealthFragment extends Fragment {
+}
