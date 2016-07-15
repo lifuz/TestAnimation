77b3f7fcad9bebac063f1608949b0555ade12307
lifuz <lifuzz@163.com>
2016-07-15 18:06:02 +0800

    登录

--- self/src/main/java/com/lifuz/self/activity/LoginActivity.java
+++ self/src/main/java/com/lifuz/self/activity/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.lifuz.self.activity;
 
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lifuz.self.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  *
@@ -20,11 +26,18 @@ import butterknife.ButterKnife;
  */
 public class LoginActivity extends BaseActivity {
 
+
+
+    private static final String TAG = "LoginActivity";
+
     @BindView(R.id.toolbar)
     protected Toolbar toolbar;
 
     @BindView(R.id.center_title)
     protected TextView centerTitle;
+
+    @BindView(R.id.qq_login_btn)
+    protected ImageButton qq_login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +50,43 @@ public class LoginActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        qq_login_btn.setFocusable(true);
+        qq_login_btn.requestFocus();
+
+        qq_login_btn.setFocusableInTouchMode(true);
+        qq_login_btn.requestFocusFromTouch();
+
+
         centerTitle.setText("登录");
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             toolbar.setElevation(0F);
         }
+
+
+    }
+
+    /**
+     * qq登录
+     */
+    @OnClick(R.id.qq_login_btn)
+    public void qqLogin() {
+
+        Log.e(TAG,"qq_login_btn is onClick");
+
+    }
+
+    @OnClick(R.id.login_btn)
+    public void login() {
+        Log.e(TAG,"Login is onclick");
+
+        InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if(im.isActive()) {
+            im.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+
 
     }
 }
