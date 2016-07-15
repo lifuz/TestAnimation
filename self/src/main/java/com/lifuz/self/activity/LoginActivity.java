c0add436d7cce5113a7bee767d001c7747ab1146
lifuz <lifuzz@163.com>
2016-07-15 21:58:05 +0800

    qq登录

--- self/src/main/java/com/lifuz/self/activity/LoginActivity.java
+++ self/src/main/java/com/lifuz/self/activity/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.lifuz.self.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
@@ -11,6 +15,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lifuz.self.R;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.common.Constants;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,6 +50,13 @@ public class LoginActivity extends BaseActivity {
     @BindView(R.id.qq_login_btn)
     protected ImageButton qq_login_btn;
 
+    @BindView(R.id.login_phone_et)
+    protected TextInputEditText login_phone_et;
+
+    private Tencent tencent;
+
+    private UserInfo userInfo = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +67,8 @@ public class LoginActivity extends BaseActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        tencent = Tencent.createInstance(getString(R.string.qq_app_id),this);
 
         qq_login_btn.setFocusable(true);
         qq_login_btn.requestFocus();
@@ -74,19 +94,94 @@ public class LoginActivity extends BaseActivity {
 
         Log.e(TAG,"qq_login_btn is onClick");
 
+        tencent.login(LoginActivity.this, "all",loginListener);
+
     }
 
     @OnClick(R.id.login_btn)
     public void login() {
         Log.e(TAG,"Login is onclick");
 
-        InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        if(im.isActive()) {
-            im.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-
-
+        imm.hideSoftInputFromWindow(login_phone_et.getWindowToken(), 0);
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Constants.REQUEST_LOGIN ||
+                requestCode == Constants.REQUEST_APPBAR) {
+            Tencent.onActivityResultData(requestCode,resultCode,data,loginListener);
+        }
+    }
+
+    public void initOpenidAndToken(JSONObject jsonObject) {
+        try {
+            String token = jsonObject.getString(Constants.PARAM_ACCESS_TOKEN);
+            String expires = jsonObject.getString(Constants.PARAM_EXPIRES_IN);
+            String openId = jsonObject.getString(Constants.PARAM_OPEN_ID);
+            if (!TextUtils.isEmpty(token) && !TextUtils.isEmpty(expires)
+                    && !TextUtils.isEmpty(openId)) {
+                tencent.setAccessToken(token, expires);
+                tencent.setOpenId(openId);
+            }
+        } catch(Exception e) {
+        }
+    }
+
+    private IUiListener loginListener = new IUiListener() {
+        @Override
+        public void onComplete(Object o) {
+
+            Log.e(TAG,"qq登录成功");
+
+            Log.e(TAG,o.toString());
+
+            initOpenidAndToken((JSONObject) o);
+
+//            tencent.requestAsync(Constants.GRAPH_SIMPLE_USER_INFO);
+
+            Log.e(TAG,tencent.getQQToken().getOpenId());
+
+
+
+            userInfo = new UserInfo(LoginActivity.this, tencent.getQQToken());
+
+            userInfo.getUserInfo(new IUiListener() {
+                @Override
+                public void onComplete(Object o) {
+                    Log.e(TAG,"获取信息成功");
+                    Log.e(TAG,o.toString());
+                }
+
+                @Override
+                public void onError(UiError uiError) {
+
+                }
+
+                @Override
+                public void onCancel() {
+
+                }
+            });
+
+        }
+
+        @Override
+        public void onError(UiError uiError) {
+
+            Log.e(TAG,"qq登录失败");
+
+        }
+
+        @Override
+        public void onCancel() {
+
+            Log.e(TAG,"取消登录");
+
+        }
+    };
 }
