b45b55b45820f5c650a0469e982466a118efb47e
lifuz <lifuzz@163.com>
2016-07-30 08:55:17 +0800

    初步使用RxJava

--- testrxjava/src/main/java/com/lifuz/testrxjava/MainActivity.java
+++ testrxjava/src/main/java/com/lifuz/testrxjava/MainActivity.java
@@ -1,13 +1,60 @@
 package com.lifuz.testrxjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        observable.subscribe(observer);
+
+//        observable.subscribe(subscriber)
+
     }
+
+    //被观察者
+    Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+        @Override
+        public void call(Subscriber<? super String> subscriber) {
+
+            //事件序列
+            subscriber.onNext("Hello");
+            subscriber.onNext("Hi");
+            subscriber.onNext("Aloha");
+            subscriber.onCompleted();
+        }
+    });
+
+
+    //观察者
+    Observer<String> observer = new Observer<String>() {
+        @Override
+        public void onCompleted() {
+
+            Log.e(TAG,"onCompleted");
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.e(TAG,"onError");
+        }
+
+        @Override
+        public void onNext(String s) {
+            Log.e(TAG,"onNext");
+        }
+    };
+
 }
